5ba2f00cf1c550cf6bd681dc8a4d965579ccb29e
Park <winkknd@naver.com>
2019-04-23 05:29:31 +0900

    어휴

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default () => (
+  <header>
+    <ul>
+      <li>
+        <a href="/">mother fucking home</a>
+      </li>
+      <li>
+        <a href="/tv">TV</a>
+      </li>
+      <li>
+        <a href="/search">search</a>
+      </li>
+    </ul>
+  </header>
+);
