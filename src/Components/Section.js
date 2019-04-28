ee783cde4727e3ea2e0b7b1366234df940171504
Park <winkknd@naver.com>
2019-04-29 02:27:53 +0900

    .

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -9,12 +9,12 @@ const Container = styled.div`
 `;
 
 const Title = styled.span`
-  font-size: 16px;
+  font-size: 14px;
   font-weight: 600;
 `;
 
 const Grid = styled.div`
-  margin-bottom: 25px;
+  margin-top: 25px;
   display: grid;
   grid-template-columns: repeat(auto-fill, 125px);
   grid-gap: 25px;
--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,10 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-
 import Section from "Components/Section";
 import Loader from "../../Components/Loader";
 import Message from "../../Components/Message";
+import Poster from "../../Components/Poster";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -15,24 +15,24 @@ const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
     <Loader />
   ) : (
     <Container>
-      {upcoming && upcoming.length > 0 && (
-        <Section title="Upcoming Movies">
-          {upcoming.map(movie => (
-            <span key={movie.id}>{movie.title}</span>
-          ))}
-        </Section>
-      )}
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title="Now Playing">
           {nowPlaying.map(movie => (
-            <span key={movie.id}>{movie.title}</span>
+            <Poster />
+          ))}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming Movies">
+          {upcoming.map(movie => (
+            <Poster />
           ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular Movies">
           {popular.map(movie => (
-            <span key={movie.id}>{movie.title}</span>
+            <Poster />
           ))}
         </Section>
       )}
@@ -43,7 +43,7 @@ const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
   popular: PropTypes.array,
-  upcomfing: PropTypes.array,
+  upcoming: PropTypes.array,
   loading: PropTypes.bool.isRequired,
   error: PropTypes.string
 };
